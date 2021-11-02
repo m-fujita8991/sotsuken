9d4f7e62ac10229b82c0abfb9363df43edb4b9c7
Saison2457 <nishimura2457@gmail.com>
2021-11-02 15:09:45 +0900

    Sampleテスト

---
+++ app/Http/Controllers/SampleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SampleController extends Controller
+{
+    public function index(){
+        return view('sample');
+    }
+
+    public function change(){
+        return view('sample2');
+    }
+}
---
+++ resources/views/sample.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<link rel="stylesheet" href="css/sample.css">
+</head>
+<body>
+<h1>Beetles</h1>
+    <h2>External morphology</h2>
+        <h3>Head</h3>
+            <h4>Mouthparts</h4>
+        <h3>Thorax</h3>
+            <h4>Prothorax</h4>
+            <h4>Pterothorax</h4>
+    <form action="" method="post">
+        {{@csrf_field()}}
+        <button type="submit">aaaaa</button>
+    </form>
+</body>
+</html>
