f055a5657d320cb1b1d65f2bda0f249c5f042547
m-fujita8991 <masasa6k@gmail.com>
2021-11-09 15:08:50 +0900

    pipi top画面

---
+++ sotsuken-top/move.js
@@ -0,0 +1,17 @@
+$('.slider').slick({
+    autoplay: true,
+    dots: false,
+    arrows: false,
+    infinite: true,
+    fade: true,
+    cssEase: 'linear'
+  });
+
+  $('.slider2').slick({
+    autoplay: true,
+    dots: false,
+    arrows: false,
+    infinite: true,
+    fade: true,
+    cssEase: 'linear'
+  });
