4edd14d0e7918960af3e37548ebe3c5b497ca828
Saison2457 <nishimura2457@gmail.com>
2021-12-13 10:48:51 +0900

    パンくず

--- resources/views/sotuken/index.blade.php
+++ resources/views/sotuken/index.blade.php
@@ -70,6 +70,76 @@
 				<input type="image" class="search_button" src="../image/index/search.png" onmouseover="this.src='../image/index/search2.png'" onmouseout="this.src='../image/index/search.png'">
 			</form>
 
+			<ol class="breadcrumb">
+			<li>
+				<span>
+					@switch($locate)
+						@case(0) 北海道・東北 @break
+						@case(1) 関東 @break
+						@case(2) 中部 @break
+						@case(3) 近畿 @break
+						@case(4) 中国・四国 @break	
+						@case(5) 九州 @break
+					@endswitch
+				</span>
+			</li>
+
+			<li>
+				<span>
+					@switch($prefecture)
+						@case(0) 全域 @break
+						@case(1) 北海道 @break
+						@case(2) 青森県 @break
+						@case(3) 岩手県 @break
+						@case(4) 宮城県 @break
+						@case(5) 秋田県 @break
+						@case(6) 山形県 @break
+						@case(7) 福島県 @break
+						@case(8) 茨城県 @break
+						@case(9) 栃木県 @break
+						@case(10) 群馬県 @break
+						@case(11) 埼玉県 @break
+						@case(12) 千葉県 @break
+						@case(13) 東京都 @break
+						@case(14) 神奈川県 @break
+						@case(15) 新潟県 @break
+						@case(16) 富山県 @break
+						@case(17) 石川県 @break
+						@case(18) 福井県 @break
+						@case(19) 山梨県 @break
+						@case(20) 長野県 @break
+						@case(21) 岐阜県 @break
+						@case(22) 静岡県 @break
+						@case(23) 愛知県 @break
+						@case(24) 三重県 @break
+						@case(25) 滋賀県 @break
+						@case(26) 京都府 @break
+						@case(27) 大阪府 @break
+						@case(28) 兵庫県 @break
+						@case(29) 奈良県 @break
+						@case(30) 和歌山県 @break
+						@case(31) 鳥取県 @break
+						@case(32) 島根県 @break
+						@case(33) 岡山県 @break
+						@case(34) 広島県 @break
+						@case(35) 山口県 @break
+						@case(36) 徳島県 @break
+						@case(37) 香川県 @break
+						@case(38) 愛媛県 @break
+						@case(39) 高知県 @break
+						@case(40) 福岡県 @break
+						@case(41) 佐賀県 @break
+						@case(42) 長崎県 @break
+						@case(43) 熊本県 @break
+						@case(44) 大分県 @break
+						@case(45) 宮崎県 @break
+						@case(46) 鹿児島県 @break
+						@case(47) 沖縄県 @break
+					@endswitch
+				</span>
+			</li>
+		</ol>
+
 			<div id="contents">
 
 				<div class="inner">
